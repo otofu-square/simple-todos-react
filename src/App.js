91e71588a06e4657b2b33c3aea8c0a9e745ed99d
otofu-square <yukita@feedforce.jp>
2018-07-24 08:33:29 +0900

    :tada: part5 finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
+        <TodoForm />
         <TodoList />
       </div>
     );
